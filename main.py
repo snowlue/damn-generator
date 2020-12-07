10003f738faca9c79045db9aafc1d610739d7473
bogtogus <75621064+bogtogus@users.noreply.github.com>
2020-12-07 18:13:48 +0300

    Изменение имён переменных
    Предлагаю всем перейти на эту версию, ща попробую пулл реквест

--- main.py
+++ main.py
@@ -2,23 +2,17 @@ from random import choice
 import pymorphy2
 
 
-# test
 morph = pymorphy2.MorphAnalyzer(lang='ru')
 
-ass = ['жопа', 'ниггер', 'принцесса', 'очко',
-       'пидор', 'Ъ', 'гей', 'босс', 'кожевник',
-       'залупа', 'мусор', 'Максим', 'Боб', 'выборы']
-pril = ['голубой', 'разорванный', 'треснувший', 'красный',
-        'продажный', 'поехавший', 'обдолбанный', 'анальный',
-        'чёрный', 'тюремный', 'анимешный', 'путинский', '']
-prinadl = ['собака', 'конь', 'слон', 'кошкодевка', 'гном']
-from_ = ['Дагестан', 'Чечня', 'Украина', 'США', 'Венесуэла', 'ФСБ']
+sush_mass = ['жопа', 'ниггер', 'очко', 'Ъ', 'гей', 'выборы']
+prilag_mass = ['голубой', 'анимешный', '']
+prinadl_mass = ['конь', 'кошкодевка']
+from_mass = ['Дагестан', 'США']
 choose = choice(['gent', 'ablt', ''])
 choose_country = choice(['gent', ''])
 
-
-words = [choice(ass)]
-p = choice(pril)
+words = [choice(sush_mass)]
+prilag = choice(prilag_mass)
 
 if words[0] == 'гей':
     gender = morph.parse(words[0])[1].tag.gender
@@ -28,20 +22,20 @@ else:
         gender = morph.parse(words[0])[0].tag.gender
         number = morph.parse(words[0])[0].tag.number
         if number == 'plur':
-            morph.parse(p)[0].inflect({number})
+            morph.parse(prilag)[0].inflect({number})
         else:
-            morph.parse(p)[0].inflect({gender})
+            morph.parse(prilag)[0].inflect({gender})
     except Exception:
         gender = 'neut'
         number = 'sing'
-if p:
+if prilag:
     if number == 'plur':
-        words = [morph.parse(p)[0].inflect({number}).word] + [words[0]]
+        words = [morph.parse(prilag)[0].inflect({number}).word] + [words[0]]
     else:
-        words = [morph.parse(p)[0].inflect({gender}).word] + [words[0]]
+        words = [morph.parse(prilag)[0].inflect({gender}).word] + [words[0]]
 
 if choose:
-    pr = choice(prinadl)
+    pr = choice(prinadl_mass)
     if morph.parse(words[-1])[0].tag.animacy == 'anim' and choose == 'ablt':
         words += ['c' if not pr.startswith('с') or pr.startswith('со') else 'co']
         words += [morph.parse(pr)[0].inflect({choose}).word]
@@ -49,7 +43,7 @@ if choose:
         words += [morph.parse(pr)[0].inflect({choose}).word]
 
 if choose_country:
-    c = morph.parse(choice(from_))[0].inflect({choose_country})
+    c = morph.parse(choice(from_mass))[0].inflect({choose_country})
     if 'Abbr' in c.tag:
         c = c.word.upper()
     elif 'Geox' in c.tag:
