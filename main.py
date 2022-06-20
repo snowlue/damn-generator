074659b4bb98a5d6679dddbbc7677939e9a78e42
Pavel Ovchinnikov <opavel@internet.ru>
2022-06-20 15:55:34 +0000

    docs&refactor: рефакторинг и документация функций
    - Прописаны докстринги к функциям
    - Поправлены аннотации типов
    - Убран транслит из названий переменных и заменён английским

--- main.py
+++ main.py
@@ -1,146 +1,163 @@
 from random import choice
-from typing import Set
+from typing import Literal
+
 import pymorphy2
 from pymorphy2.analyzer import Parse
 
 
-def get_tag(word: str, tag: str, num_in_list: int = 0):
+def get_tag(word: str, tag: Literal['род', 'число', 'одуш', 'падеж'], num_in_list: int = 0) -> None | str:
+    """Выдаёт значение выбранного тега tag для слова word из базы pymorphy2.
+
+    Параметры
+    =========
+    word: str
+        слово, для которого осуществляется поиск тег
+    tag: Literal['род', 'число', 'одуш', 'падеж']
+        тег, характеризующий слово
+    num_in_list: int = 0
+        номер по порядку в списке результатов парсера pymorphy2
     """
-    (слово, тег, номер в списке рез-ов) -> значение тега
-    """
+
     if tag == 'род':
         return morph.parse(word)[num_in_list].tag.gender
-    elif tag == 'число':
+    if tag == 'число':
         return morph.parse(word)[num_in_list].tag.number
-    elif tag == 'одуш':
+    if tag == 'одуш':
         return morph.parse(word)[num_in_list].tag.animacy
-    elif tag == 'падеж':
+    if tag == 'падеж':
         return morph.parse(word)[num_in_list].tag.case
+    return None
 
 
-def inflector(word: str or Parse, tags: Set[str], num_in_list: int = 0):
-    """
-    (слово | объект Parse, теги, номер в списке рез-ов) -> объект Parse
+def inflector(word: str | Parse, tags: set[str], num_in_list: int = 0) -> Parse:
+    """Возвращает объект Parse, в котором содержится слово word, склонённое по тегам tags.
+
+    Параметры
+    =========
+    word: str | Parse
+        слово или объект выдачи morph.parse()
+    tag: set[str]
+        множество тегов, по которым нужно просклонять слово
+    num_in_list: int = 0
+        номер по порядку в списке результатов парсера pymorphy2
     """
     if isinstance(word, Parse):
         return word.inflect(tags)
-    else:
-        return morph.parse(word)[num_in_list].inflect(tags)
+    return morph.parse(word)[num_in_list].inflect(tags)
+
 
 morph = pymorphy2.MorphAnalyzer(lang='ru')
 
 # =====================================================
-sush_mass = ['жопа', 'ниггер', 'очко', 'Ъ', 'выборы', 'ублюдки', 'Фёдор',
-             'Максим', 'мразь', 'пидор', 'мошонка',
-             'анимешник', 'импотент', 'говно', 'мать Максима', 'спермотоксикозник', 
-             'транссексуал', 'уёбок', 'Барак Обама']
-prilag_mass = ['голубой', 'анимешный', 'тупой', 'карликовый',
-               'опущенный', 'голодный', 'новогодний', 'каловый', 'засранный']
-prinadl_mass = ['шаболда', 'лошадь', 'кошкодевка', 'бомжи',
-                'говно', 'рабы', 'украинцы', 'чеченцы', '']
-from_mass = ['Дагестан', 'США', 'пещера', 'помойка',
-             'наркопритон', 'рабство', 'подвал', 'хата Феди', 'ноготь', 'Гейропа',
-             'хрущёвка', '']
-glagol_mass = ['толкать', 'уничтожать', 'долбиться',
-               'насиловать', 'жрать', 'бить', 'унижать',
-               'заебаться', 'обосраться на', 'карать', 'дрочить на', 
-               'пытать', 'ссать на', 'блевать на']
+nouns = ['жопа', 'ниггер', 'очко', 'Ъ', 'выборы', 'ублюдки', 'Фёдор',
+         'Максим', 'мразь', 'пидор', 'мошонка',
+         'анимешник', 'импотент', 'говно', 'мать Максима', 'спермотоксикозник',
+         'транссексуал', 'уёбок', 'Барак Обама']
+adjectives = ['голубой', 'анимешный', 'тупой', 'карликовый',
+              'опущенный', 'голодный', 'новогодний', 'каловый', 'засранный']
+affiliation = ['шаболда', 'лошадь', 'кошкодевка', 'бомжи',
+               'говно', 'рабы', 'украинцы', 'чеченцы', '']
+homes = ['Дагестан', 'США', 'пещера', 'помойка',
+         'наркопритон', 'рабство', 'подвал', 'хата Феди', 'ноготь', 'Гейропа',
+         'хрущёвка', '']
+verbs = ['толкать', 'уничтожать', 'долбиться',
+         'насиловать', 'жрать', 'бить', 'унижать',
+         'заебаться', 'обосраться на', 'карать', 'дрочить на',
+         'пытать', 'ссать на', 'блевать на']
 
 
 def generate_one():
     # gent — родит., ablt - творит., accs - винит., accs - предл.
-    padezh_prinadl = choice(['gent', 'ablt'])
-    padezh_from = choice(['gent', 'loct'])
+    case_affil, case_home = choice(['gent', 'ablt']), choice(['gent', 'loct'])
 
-    words, prilag = [choice(sush_mass)], choice(prilag_mass)
-    prinadl, glagol, from_ = choice(prinadl_mass), choice(glagol_mass), choice(from_mass)
-    # words, prilag, prinadl, glagol, from_  = ['ниггер'], 'тупой', 'рабы', 'обосраться', 'наркопритон'
-    sush = words[0]
+    words, adj = [choice(nouns)], choice(adjectives)
+    affil, verb, home = choice(affiliation), choice(verbs), choice(homes)
+    noun = words[0]
 
     # СБОР ИНФОРМАЦИИ О СУЩЕСТВИТЕЛЬНОМ
-    sush = sush.split()[0] if len(sush.split()) > 1 else sush
-    gender = get_tag(sush, 'род')
-    number = get_tag(sush, 'число')
+    noun = noun.split()[0] if len(noun.split()) > 1 else noun
+    gender = get_tag(noun, 'род')
+    number = get_tag(noun, 'число')
     try:
         if number == 'plur':
-            inflector(prilag, {number})
+            inflector(adj, {number})
         else:
-            inflector(prilag, {gender})
+            inflector(adj, {gender})
     except Exception:
         gender, number = 'neut', 'sing'
 
     # ДОБАВЛЕНИЕ ПРИЛАГАТЕЛЬНОГО
-    if prilag:  # если есть слово
-        prilag_parser = morph.parse(prilag)[0]
+    if adj:  # если есть слово
+        adj_parser = morph.parse(adj)[0]
         if number == 'plur':
-            words = [inflector(prilag_parser, {number, 'nomn'}).word.capitalize()] + [words[0]]
+            words = [inflector(adj_parser, {number, 'nomn'}).word.capitalize()] + [words[0]]
         else:
-            words = [inflector(prilag_parser, {gender, 'nomn'}).word.capitalize()] + [words[0]]
+            words = [inflector(adj_parser, {gender, 'nomn'}).word.capitalize()] + [words[0]]
     else:
         capital = words[0].split()
         words = [capital[0].capitalize()] + capital[1:]
 
     # ВЫЧИСЛЕНИЕ ПРИНАДЛЕЖНОСТИ
-    if from_:
-        from_ = [from_.split()[0], *from_.split()[1:]] if len(from_.split()) > 1 else [from_]
-        ffrom_ = inflector(from_[0], {padezh_from})
-        if 'Abbr' in ffrom_.tag:
-            ffrom_ = ffrom_.word.upper()
-        elif 'Geox' in ffrom_.tag:
-            ffrom_ = ffrom_.word.capitalize()
+    if home:
+        home = [home.split()[0], *home.split()[1:]] if len(home.split()) > 1 else [home]
+        home_inf = inflector(home[0], {case_home})
+        if 'Abbr' in home_inf.tag:
+            home_inf = home_inf.word.upper()
+        elif 'Geox' in home_inf.tag:
+            home_inf = home_inf.word.capitalize()
         else:
-            ffrom_ = ffrom_.word
+            home_inf = home_inf.word
 
-        if padezh_from == "gent":
-            if ffrom_ == 'ногтя':
-                locate = ['из-под ' + ffrom_] + from_[1:]
+        if case_home == "gent":
+            if home_inf == 'ногтя':
+                locate = ['из-под ' + home_inf] + home[1:]
             else:
-                locate = ['из ' + ffrom_] + from_[1:]
-        elif padezh_from == "loct":
-            locate = ['в ' + ffrom_] + from_[1:]
+                locate = ['из ' + home_inf] + home[1:]
+        elif case_home == "loct":
+            locate = ['в ' + home_inf] + home[1:]
 
     # ДОБАВЛЕНИЕ ГЛАГОЛА И ПРИНАДЛЕЖНОСТИ
-    if prinadl:  # если есть падеж, значит есть и слово
-        if get_tag(sush, 'одуш') == 'anim' and padezh_prinadl == 'ablt':
-            if from_:
+    if affil:  # если есть падеж, значит есть и слово
+        if get_tag(noun, 'одуш') == 'anim' and case_affil == 'ablt':
+            if home:
                 words += locate
-            if glagol:
-                glagol, pf = (glagol.split()[0], glagol.split()[1]) if len(glagol.split()) == 2 else (glagol, '')
+            if verb:
+                verb, prepos = (verb.split()[0], verb.split()[1]) if len(verb.split()) == 2 else (verb, '')
                 if number == 'plur':
-                    if glagol.endswith('ся'):
-                        words += [inflector(glagol, {number}).word]
+                    if verb.endswith('ся'):
+                        words += [inflector(verb, {number}).word]
                     else:
-                        words += [inflector(glagol, {number, '3per'}).word]
+                        words += [inflector(verb, {number, '3per'}).word]
                 else:
-                    if glagol.endswith('ся'):
-                        words += [inflector(glagol, {gender}).word]
+                    if verb.endswith('ся'):
+                        words += [inflector(verb, {gender}).word]
                     else:
-                        words += [inflector(glagol, {'3per'}).word]
+                        words += [inflector(verb, {'3per'}).word]
 
-                if 'intr' in morph.parse(glagol)[0].tag:  # совершенный вид
-                    if pf:
-                        words += [pf]
-                        if prinadl != 'анимешники':
-                            words += [inflector(prinadl, {'accs'}).word]
+                if 'intr' in morph.parse(verb)[0].tag:  # совершенный вид
+                    if prepos:
+                        words += [prepos]
+                        if affil != 'анимешники':
+                            words += [inflector(affil, {'accs'}).word]
                         else:
-                            words += [inflector(prinadl, {'gent'}).word]
+                            words += [inflector(affil, {'gent'}).word]
                     else:
-                        words += ['c' if not prinadl.startswith('с') or prinadl.startswith('со') else 'co']
-                        words += [inflector(prinadl, {'ablt'}).word]
-                elif 'tran' in morph.parse(glagol)[0].tag:
-                    if pf:
-                        words += [pf]
-                    if prinadl != 'анимешники':
-                        words += [inflector(prinadl, {'accs'}).word]
+                        words += ['c' if not affil.startswith('с') or affil.startswith('со') else 'co']
+                        words += [inflector(affil, {'ablt'}).word]
+                elif 'tran' in morph.parse(verb)[0].tag:
+                    if prepos:
+                        words += [prepos]
+                    if affil != 'анимешники':
+                        words += [inflector(affil, {'accs'}).word]
                     else:
-                        words += [inflector(prinadl, {'gent'}).word]
+                        words += [inflector(affil, {'gent'}).word]
             else:  # нет глагола
-                words += ['c' if not prinadl.startswith('с') or prinadl.startswith('со') else 'co']
-                words += [inflector(prinadl, {'ablt'}).word]
-        #elif get_tag(sush, 'одуш') == 'anim' and padezh_prinadl == 'gent' and glagol:
-        elif get_tag(sush, 'одуш') == 'inan' and padezh_prinadl == 'gent':
-            words += [inflector(prinadl, {padezh_prinadl}).word]
-            if from_:
+                words += ['c' if not affil.startswith('с') or affil.startswith('со') else 'co']
+                words += [inflector(affil, {'ablt'}).word]
+        # elif get_tag(sush, 'одуш') == 'anim' and case_affil == 'gent' and glagol:
+        elif get_tag(noun, 'одуш') == 'inan' and case_affil == 'gent':
+            words += [inflector(affil, {case_affil}).word]
+            if home:
                 words += locate
     return ' '.join(words)
 
